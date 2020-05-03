66ee9937bf9c5d24d224740273888c75eab68c00
rbcurry <25296330+rbcurry@users.noreply.github.com>
2020-05-03 18:23:49 +1000

    Updated 2020-05-03

--- step4-corvif.R
+++ step4-corvif.R
@@ -19,10 +19,10 @@ corr <- function(data) {
 
 # Calculate correlation table
 ef.corr <- corr(epfu)
-write.csv(ef.corr, "./results/epfu-corr1.csv")
+write.csv(ef.corr, "./results/corvif/epfu-corr1.csv")
 
 ms.corr <- corr(myse)
-write.csv(ms.corr, "./results/myse-corr1.csv")
+write.csv(ms.corr, "./results/corvif/myse-corr1.csv")
 
 # Remove correlated variables
 
@@ -41,7 +41,7 @@ epfu$pdev <- NULL
 
 # Re-calculate correlation table
 ef.corr <- corr(epfu)
-write.csv(ef.corr, "./results/epfu-corr2.csv")
+write.csv(ef.corr, "./results/corvif/epfu-corr2.csv")
 
 # ptcc vs. pfor
 ptcc <- glmer(use ~ ptcc + (1|batid), family = "binomial", data = epfu)
@@ -56,7 +56,7 @@ epfu$pfor <- NULL
 
 # Re-calculate correlation table
 ef.corr <- corr(epfu)
-write.csv(ef.corr, "./results/epfu-corr3.csv")
+write.csv(ef.corr, "./results/corvif/epfu-corr3.csv")
 
 # bldg vs. ptcc
 bldg <- glmer(use ~ bldg + (1|batid), family = "binomial", data = epfu)
@@ -71,7 +71,7 @@ epfu$bldg <- NULL
 
 # Re-calculate correlation table
 ef.corr <- corr(epfu)
-write.csv(ef.corr, "./results/epfu-corr4.csv")
+write.csv(ef.corr, "./results/corvif/epfu-corr4.csv")
 
 # MYSE
 
@@ -88,8 +88,73 @@ myse$pdev <- NULL
 
 # Re-calculate correlation table
 ms.corr <- corr(myse)
-write.csv(ms.corr, "./results/myse-corr2.csv")
+write.csv(ms.corr, "./results/corvif/myse-corr2.csv")
 
 # Save
 save(epfu, file = "./data/epfu2.Rdata")
 save(myse, file = "./data/myse2.Rdata")
+
+# Generate global models and examine VIF
+
+# VIF function
+vif.mer <- function (fit) {
+    ## adapted from rms::vif
+    v <- vcov(fit)
+    nam <- names(fixef(fit))
+    ## exclude intercepts
+    ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
+    if (ns > 0) {
+        v <- v[-(1:ns), -(1:ns), drop = FALSE]
+        nam <- nam[-(1:ns)]
+    }
+    d <- diag(v)^0.5
+    v <- diag(solve(v/(d %o% d)))
+    names(v) <- nam
+    v
+}
+
+# EPFU
+load("./data/epfu2.Rdata")
+vars <- names(epfu[9:length(names(epfu))])
+m <- list()
+
+for (i in 1:length(vars)) {
+    m[[i]] <- glmer(
+        formula(paste("use ~ ", vars[i], " + (1|batid)", sep = "")), 
+        family = "binomial", data = epfu)
+}
+rm(i)
+
+# Generate VIF table
+vif.table <- data.frame(row.names = vars, VIF = sapply(m, vif.mer))
+vif.table
+vif.table[which(vif.table$VIF >= 4), ] # None
+
+# Write table
+write.csv(vif.table, file = "./results/corvif/epfu-vif.csv")
+
+# Cleanup
+rm(epfu, vars, m, vif.table)
+
+# MYSE
+load("./data/myse2.Rdata")
+vars <- names(myse[9:length(names(myse))])
+m <- list()
+
+for (i in 1:length(vars)) {
+    m[[i]] <- glmer(
+        formula(paste("use ~ ", vars[i], " + (1|batid)", sep = "")), 
+        family = "binomial", data = myse)
+}
+rm(i)
+
+# Generate VIF table
+vif.table <- data.frame(row.names = vars, VIF = sapply(m, vif.mer))
+vif.table
+vif.table[which(vif.table$VIF >= 4), ] # None
+
+# Write table
+write.csv(vif.table, file = "./results/corvif/myse-vif.csv")
+
+# Cleanup
+rm(myse, vars, m, vif.table)
